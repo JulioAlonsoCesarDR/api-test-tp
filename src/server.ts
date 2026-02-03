6bd84be6808e984c8a2f1e0185553773a6e77f55
jadiazr1 <jadiazr@businesspro.mx>
2026-02-03 14:03:54 -0600

    added config CORS

--- src/server.ts
+++ src/server.ts
@@ -2,7 +2,9 @@ import express from 'express';
 import colors from 'colors';
 import db from './config/db';
 import router from './router';
-
+import cors, { CorsOptions } from 'cors';
+import dotenv from 'dotenv'
+dotenv.config()
 
 // Conectar a base de datos
 async function connectDB() {
@@ -11,6 +13,7 @@ async function connectDB() {
         db.sync()
         console.log( colors.blue( 'Conexión exitosa a la BD'))
     } catch (error) {
+        console.log(colors.red.bold(error))
         console.log( colors.red.bold( 'Hubo un error al conectar a la BD') )
     }
 }
@@ -19,9 +22,19 @@ connectDB()
 // Instancia de express
 const server = express()
 
+const corsOptions: CorsOptions = {
+    origin: function(origin, callback) {
+        if (origin === process.env.FRONTEND_URL) {
+            callback(null, true)
+        } else {
+            callback(new Error('No permitido por CORS'))
+        }
+     }
+};
+server.use(cors(corsOptions))
+
 // Leer datos de formularios
 server.use(express.json())
-
 server.use('/api/address', router)
 
 export default server
